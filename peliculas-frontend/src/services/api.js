a1d65399fb4d272e8a5040e17f657ae0da4c6172
Alexander Fonnegra Jiménez <fonegra2015@gmail.com>
2025-10-05 10:54:28 -0500

    ..

--- peliculas-frontend/src/services/api.js
+++ peliculas-frontend/src/services/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://evidencia-de-aprendizaje-2-front-end-r8rk.onrender.com/api"
+    : "http://localhost:3000/api";
+
 const api = axios.create({
-  baseURL: "https://NOMBRE-DEL-BACKEND.onrender.com/api"
+  baseURL,
 });
 
 export default api;
