ee8fd2043559e8665ee309bfb8da28101e1e081c
Alexander Fonnegra Jiménez <fonegra2015@gmail.com>
2025-10-05 10:17:06 -0500

    .

--- PeliculasBackend/src/config/database.js
+++ PeliculasBackend/src/config/database.js
@@ -4,25 +4,33 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.DB_DIALECT === 'sqlite') {
-  // Conexión a SQLite
+  // Conexión local con SQLite (modo desarrollo)
   sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: process.env.DB_STORAGE || './database.sqlite',
     logging: false
   });
-} else {
-
+} else if (process.env.DB_DIALECT === 'postgres') {
+  // Conexión a PostgreSQL (modo producción en Render)
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASS,
     {
       host: process.env.DB_HOST,
-      dialect: 'mysql',
-      port: process.env.DB_PORT || 3306,
-      logging: false
+      port: process.env.DB_PORT || 5432,
+      dialect: 'postgres',
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
     }
   );
+} else {
+  throw new Error('DB_DIALECT no configurado correctamente en .env');
 }
 
 module.exports = sequelize;
